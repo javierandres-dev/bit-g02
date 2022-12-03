27b5dd81f6429564868cb3fdacbc4f7921eba905
javierandresgp <info@javierandres.dev>
2022-12-02 22:27:59 -0500

    js

--- 04javascript/js/exercises.js
+++ 04javascript/js/exercises.js
@@ -1,52 +1,37 @@
 'use strict';
-// 1. Pedir nombre al usuario y saludarlo con alerta
+// 1. Pedir nombre al usuario y saludarlo
+function saludar() {
+  const nombre = prompt('Escribe tu nombre');
+  if (nombre === null) {
+    alert(':( No escribiste tu nombre.');
+  } else {
+    alert('Hola, ' + nombre);
+  }
+}
+//saludar();
+
 // 2. Pedir edad al usuario y responder si es o no es mayor de edad
-/* 3. Pedir nombre y edad al usuario, usando la estructura "switch",
-si se llama Pepita y es mayor de edad permitir ingreso a sala 1,
-si se llama Pepita y es menor de edad permitir ingreso a sala 2,
-si no se llama Pepita y es mayor de edad permitir ingreso a sala 3,
-sino dirigir a la sala 4
-*/
-const nombre = prompt('Ingrese su nombre:');
-const edad = Number(prompt('Ingrese su edad:'));
-let esPepita;
-let esMayorDeEdad;
+function esMayorDeEdad() {
+  const edad = Number(prompt('Escribe tu edad'));
+  edad >= 18 ? alert('Eres mayor de edad') : alert('Eres menor de edad');
+}
+//esMayorDeEdad();
 
-if (nombre === 'Pepita') {
-  esPepita = true;
-} else {
-  esPepita = false;
-}
+/* 3. Pedir nombre y edad al usuario,
+si se llama Pepita y es mayor de edad permitir ingreso a sala de espera 1,
+si se llama Pepita y es menor de edad permitir ingreso a sala de espera 2,
+si no se llama Pepita pero es mayor de edad permitir ingreso a sala de espera 3,
+sino dirigir a la sala de espera 4 */
 
-if (edad >= 18) {
-  esMayorDeEdad = true;
-} else {
-  esMayorDeEdad = false;
+// Pedir un número y mostrar los numeros pares desde 1 hasta número ingresado
+function multiplosDeDos() {
+  const numero = parseInt(prompt('Ingresa un número'));
+  let pares = '';
+  for (let i = 1; i <= numero; i++) {
+    if (i % 2 === 0) {
+      pares += i + ', ';
+    }
+  }
+  alert(`Los números pares desde 1 hasta ${numero}, son: ${pares}`);
 }
-
-let destino = null;
-
-if (esPepita && esMayorDeEdad) {
-  destino = 1;
-}
-if (esPepita && !esMayorDeEdad) {
-  destino = 2;
-}
-if (!esPepita && esMayorDeEdad) {
-  destino = 3;
-}
-
-switch (destino) {
-  case 1:
-    console.log(`Ingresa a la sala #${destino}`);
-    break;
-  case 2:
-    console.log('Ingresa a la sala #' + destino);
-    break;
-  case 3:
-    console.log(`Ingresa a la sala #${destino}`);
-    break;
-  default:
-    console.log('Ingresa a la sala #4');
-    break;
-}
+//multiplosDeDos();
