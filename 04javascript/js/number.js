053603961595b1784ce3f3f6efd8f51025ea186d
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2022-12-15 22:12:39 -0500

    js

---
+++ 04javascript/js/number.js
@@ -0,0 +1,113 @@
+const valorMinimo = Number.MIN_VALUE;
+const valorMaximo = Number.MAX_VALUE;
+
+const valorEnteroMinSeguro = Number.MIN_SAFE_INTEGER;
+const valorEnteroMaxSeguro = Number.MAX_SAFE_INTEGER;
+
+const negativoInfinito = Number.NEGATIVE_INFINITY;
+const positivoInfinito = Number.POSITIVE_INFINITY;
+
+// console.log(valorMinimo);
+// console.log(valorMaximo);
+// console.log(valorEnteroMinSeguro);
+// console.log(valorEnteroMaxSeguro);
+// console.log(negativoInfinito);
+// console.log(positivoInfinito);
+
+//console.log(Number.EPSILON);
+
+const limiteCorrecto = 999999999999999;
+const limiteError = 9999999999999999;
+const decimalesMaximos = 1.2345678901234567;
+const operacionDecimal = 0.2 + 0.1;
+const operacionDecimalExacta = (0.2 * 10 + 0.1 * 10) / 10;
+
+// console.log(limiteCorrecto);
+// console.log(limiteError);
+// console.log(decimalesMaximos);
+// console.log(operacionDecimal);
+// console.log(operacionDecimalExacta);
+
+const objeto = new Number(10);
+const nan = NaN;
+const infinitoNegativo = -Infinity;
+const infinitoPositivo = Infinity;
+
+// console.log(objeto);
+// console.log(typeof objeto);
+// console.log(objeto.valueOf());
+// console.log(nan);
+// console.log(typeof nan);
+// console.log(infinitoNegativo);
+// console.log(typeof infinitoNegativo);
+// console.log(infinitoPositivo);
+// console.log(typeof infinitoPositivo);
+
+// console.log(Number.isNaN(str));
+// console.log(isNaN(str));
+// console.log(Number.isNaN(entero));
+// console.log(isNaN(entero));
+
+const str = 'Soy un string';
+const strEntero = '10';
+const strDecimal = '10.123';
+const entero = 10;
+const decimal = 10.123;
+const negativo = -10;
+const negativoDecimal = -10.123;
+const resultadoSuma = 10 + 10;
+const resultadoOperacion = 10 * 10;
+const ceroDivididoCero = 0 / 0;
+const uno = 1;
+const diez = 10;
+const millon = 1000000;
+const azucar = 1_000_000;
+
+// console.log(str);
+// console.log(typeof str);
+// console.log(entero);
+// console.log(typeof entero);
+// console.log(decimal);
+// console.log(typeof decimal);
+// console.log(negativo);
+// console.log(typeof negativo);
+// console.log(negativoDecimal);
+// console.log(typeof negativoDecimal);
+// console.log(resultadoSuma);
+// console.log(typeof resultadoSuma);
+// console.log(resultadoOperacion);
+// console.log(typeof resultadoOperacion);
+
+// console.log(Number.isSafeInteger(entero));
+// console.log(Number.isSafeInteger(valorEnteroMaxSeguro));
+// console.log(Number.isSafeInteger(valorEnteroMaxSeguro + 1));
+// console.log(Number.isSafeInteger(infinitoPositivo));
+
+// console.log(Number.isFinite(entero));
+// console.log(Number.isFinite(ceroDivididoCero));
+// console.log(Number.isFinite(infinitoPositivo));
+
+// console.log(ceroDivididoCero);
+// console.log(typeof ceroDivididoCero);
+
+// console.log(uno);
+// console.log(typeof uno);
+// console.log(diez);
+// console.log(typeof diez);
+// console.log(millon);
+// console.log(typeof millon);
+// console.log(azucar);
+// console.log(typeof azucar);
+
+// console.log(Number.isInteger(entero));
+// console.log(Number.isInteger(decimal));
+// console.log(parseInt(strEntero));
+// console.log(parseFloat(strDecimal));
+// console.log(decimal.toFixed());
+// console.log(decimal.toFixed(1));
+// console.log(decimal.toFixed(2));
+// console.log(decimal.toFixed(10));
+// console.log(entero.toString());
+// console.log(entero.toString(2));
+// console.log(entero.toString(8));
+// console.log(entero.toString(16));
