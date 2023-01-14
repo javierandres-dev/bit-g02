7191a32d1d693f94b493b0df8e4de69d5fc2ab17
javierandresgp <info@javierandres.dev>
2023-01-13 22:29:47 -0500

    js

---
+++ 04javascript/js/array.js
@@ -0,0 +1,35 @@
+'use strict';
+const arrObjVacio = new Array();
+const arregloVacio = [];
+const arrObj = new Array('elemento1', 'elemento2', 'elemento3');
+const girls = ['ana', 'shopia', 'pepita'];
+const boys = ['jon', 'pepito', 'eric'];
+const arrAny = [1, '1', 'one', true, [], {}];
+const arr = ['el1', 'el2', 'el3', 'el4'];
+
+// console.log(arrObjVacio);
+// console.log(arregloVacio);
+// console.log(arrObj);
+// console.log(girls);
+// console.log(arrAny);
+// console.log(arr);
+
+// console.log(arr);
+// console.log(typeof arr);
+// console.log(arr instanceof Array);
+
+// console.log(arregloVacio.length);
+// console.log(arr.length);
+
+// console.log(arr);
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// console.log(arr[-1]);
+// console.log(arr[4]);
+// console.log(arr[arr.length - 1]);
+
+// console.log(girls);
+// console.log(boys);
+// console.log(girls.concat(boys));
---
+++ 04javascript/js/boolean.js
@@ -0,0 +1,28 @@
+'use strict';
+const verdadero = true;
+const falso = false;
+const vObj = new Boolean(true);
+const fObj = new Boolean(false);
+let resultado = 'JavaScript' === 'Java';
+
+// console.log(verdadero);
+//console.log(falso);
+//console.log(vObj);
+// console.log(fObj);
+// console.log(resultado);
+
+// console.log(verdadero.valueOf());
+// console.log(false.valueOf());
+// console.log(vObj.valueOf());
+// console.log(fObj.valueOf());
+
+// resultado = 1 < 10;
+// console.log(resultado);
+// resultado = 1 > 10;
+// console.log(resultado);
+
+// console.log(verdadero);
+// console.log(typeof verdadero);
+// resultado = verdadero.toString();
+// console.log(resultado);
+// console.log(typeof resultado);
