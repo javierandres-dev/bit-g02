a4112361fd69d74a0445eb50d5269fd939a31118
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2023-02-22 22:02:45 -0500

    react exercise

--- 07reactExamples/exercises/src/components/ExercisesPage.jsx
+++ 07reactExamples/exercises/src/components/ExercisesPage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import { Ex01 } from './Ex01';
 import { Ex02 } from './Ex02';
+import { Ex03 } from "./Ex03";
 
 export const ExercisesPage = () => {
   const [exercise, setExercise] = useState(null);
@@ -13,6 +14,7 @@ export const ExercisesPage = () => {
         <ol>
           <li onClick={() => setExercise(<Ex01 />)}>Contador</li>
           <li onClick={() => setExercise(<Ex02 />)}>Patos</li>
+          <li onClick={() => setExercise(<Ex03 />)}>Directorio</li>
         </ol>
       ) : (
         <div className='text-center'>
